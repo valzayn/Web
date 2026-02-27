45dc6038645652b8d5b2a57ccbb74b9304a34591
valzayn <76012455+valzayn@users.noreply.github.com>
2026-02-27 19:52:09 +1000

    Add files via upload

---
+++ 2_SEM/Lab_1/JavaScript/table.js
@@ -0,0 +1,40 @@
+const createTable = (data, idTable) => {
+    const table = document.getElementById(idTable);
+    const header = Object.keys(data[0]);
+   
+    /* создание шапки таблицы */
+    const headerRow = createHeaderRow(header);
+    table.append(headerRow);
+	
+    /* создание тела таблицы */
+	const bodyRows = createBodyRows(data);
+    table.append(bodyRows);
+};
+
+const createHeaderRow = (headers) => {
+    const tr = document.createElement('tr');
+    headers.forEach(header => {
+        const th = document.createElement('th');
+        th.innerHTML = header;
+        tr.append(th);
+    });
+    return tr;
+};
+
+const createBodyRows = (data) => {
+    const tbody = document.createElement('tbody');
+    data.forEach(elem => {
+        const tr = document.createElement('tr');
+        Object.values(elem).forEach(value => {
+            const td = document.createElement('td');
+            td.innerHTML = value;
+            tr.append(td);
+        });
+        tbody.append(tr);
+    });
+    return tbody;
+};
+
+const clearTable = (idTable) => {
+    document.getElementById(idTable).innerHTML = '';
+}
